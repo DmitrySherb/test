da03756fceb104579e11e62fd36c27bf51b5ee5d
DmitrySher <dima.shcherbinin.2018gg@mail.ru>
2023-06-11 20:30:27 +0700

    Fix all

--- app/src/main/java/com/example/testapp2/MainActivity.kt
+++ app/src/main/java/com/example/testapp2/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onPause() {
         super.onPause()
-        Log.d("DimasLog","Димас крутой программас")
+        Log.d("DimasLoh","Димас крутой программас")
     }
 
     override fun onResume() {
