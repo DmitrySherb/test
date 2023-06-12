c14d8d96d38b368f9d3b965e301199bb76dc58a7
DmitrySher <dima.shcherbinin.2018gg@mail.ru>
2023-06-13 00:33:21 +0700

    Сделал обработку нажатия кнопок

--- app/src/main/java/com/example/testapp2/MainActivity.kt
+++ app/src/main/java/com/example/testapp2/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.testapp2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.text.Editable
 import android.util.Log
+import android.view.View
 import com.example.testapp2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,26 +14,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        Log.d("DimasLog","Димас не крутоgfhftghfghjй программас")
+
+        binding.googleButton.setOnClickListener {
+            Log.d("DimasLog", "Димас не крутой программас")
+        }
+        binding.loginButton.setOnClickListener {
+            val email: String = binding.et1.editText?.text.toString()
+            val password: String = binding.et2.editText?.text.toString()
+            Log.d("DimasLog", "Email:$email")
+            Log.d("DimasLog", "Password:$password")
+        }
     }
+
 
     override fun onPause() {
         super.onPause()
-        Log.d("DimasLog","Димас очень крутой программас")
+        Log.d("DimasLog", "Димас очень крутой программас")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("DimasLog","Димас крутой программас")
+        Log.d("DimasLog", "Димас крутой программас")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d("DimasLog","Димас крутой программас")
+        Log.d("DimasLog", "Димас крутой программас")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("DimasLog","Димас крутой программас")
+        Log.d("DimasLog", "Димас крутой программас")
     }
 }
